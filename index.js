e08b23d0f480946568968c9f69432ab71aa69cff
Haskusayt <80350586+Haskusayt@users.noreply.github.com>
2024-06-20 19:27:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 let gem = document.querySelector('.gem-cost')
 let gemsperclick = 1
 let clickerCost = document.querySelector('.clicker-cost')
+let standartcost = 10
 
 function incrementGem() {
   gem.innerHTML = parseFloat(gem.innerHTML) + gemsperclick
 }
 
 function buyClick() {
-  if (parseFloat(gem.innerHTML) >= clickerCost.innerHTML) {
-    parseFloat(gem.innerHTML) -= clickerCost.innerHTML
+  if (parseFloat(gem.innerHTML) >= standartcost) {
+    parseFloat(gem.innerHTML) -= standartcost
+    standartcost = standartcost ^ 2
     gemsperclick += 1
+    clickerCost = standartcost
   }
 }
